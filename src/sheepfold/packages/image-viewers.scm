0449da32ee61c26ed289bca4a7f9a646946af076
Wade Zhang <dochang@gmail.com>
2023-03-27 19:12:55 +0800

    sheepfold: nsxiv-extra: Update to 0-2.eb9c24b.
    * sheepfold/packages/image-viewers.scm (nsxiv-extra): Update to 0-2.eb9c24b.

--- src/sheepfold/packages/image-viewers.scm
+++ src/sheepfold/packages/image-viewers.scm
@@ -4,30 +4,39 @@
   #:use-module (guix build-system copy))
 
 (define-public nsxiv-extra
-  (let ((commit "ad39c6be5e8041f069f19838dc0ee481dd2e650e")
-        (revision "1"))
+  (let ((commit "eb9c24b8a89bd9c58bf3238ffae7d7f385984768")
+        (revision "2"))
     (package
       (name "nsxiv-extra")
       (version (git-version "0" revision commit))
       (source (origin
                 (method git-fetch)
                 (uri (git-reference
-                      (url "https://github.com/nsxiv/nsxiv-extra")
+                      (url "https://codeberg.org/nsxiv/nsxiv-extra.git")
                       (commit commit)))
                 (file-name (git-file-name name version))
                 (sha256
-                 (base32 "089i64a6d1sgwhij06vdlqc1aby7l8rkjr1i2w0dihi5s340lbfy"))))
+                 (base32 "13zjspxlsz70h3cw7vl8q9m2ajlh4mda29fqwin5m13xvd3r7czv"))))
       (build-system copy-build-system)
       (arguments
        `(#:install-plan
          '(,@(apply append
                     (map (lambda (script)
                            (list
-                            (list (simple-format #f "scripts/~A/~A" script script) "bin/")
+                            (list (simple-format #f "scripts/~A/~A" script script)
+                                  "bin/")
                             (list (simple-format #f "scripts/~A/README.md" script)
                                   (simple-format #f "share/doc/nsxiv-extra/~A/" script))))
-                         '("nsxiv-env" "nsxiv-pipe" "nsxiv-rifle" "nsxiv-saver" "nsxiv-thumb" "nsxiv-url"))))))
-      (home-page "https://github.com/nsxiv/nsxiv-extra")
+                         '("nsxiv-anti-alias"
+                           "nsxiv-env"
+                           "nsxiv-fill"
+                           "nsxiv-open"
+                           "nsxiv-pipe"
+                           "nsxiv-rifle"
+                           "nsxiv-saver"
+                           "nsxiv-thumb"
+                           "nsxiv-url"))))))
+      (home-page "https://codeberg.org/nsxiv/nsxiv-extra")
       (synopsis "Extra scripts for nsxiv")
       (description "This package provides scripts which add new and commonly
 requested functionality to @code{nsxiv}.")

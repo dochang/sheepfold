1da2a136536e3c96b8d5bbba68529d95a40442ae
Wade Zhang <dochang@gmail.com>
2023-04-25 02:43:55 +0800

    sheepfold: nsxiv-extra: Fix dir names in `share/doc/`.

--- src/sheepfold/packages/image-viewers.scm
+++ src/sheepfold/packages/image-viewers.scm
@@ -1,7 +1,8 @@
 (define-module (sheepfold packages image-viewers)
   #:use-module (guix packages)
   #:use-module (guix git-download)
-  #:use-module (guix build-system copy))
+  #:use-module (guix build-system copy)
+  #:use-module (guix gexp))
 
 (define-public nsxiv-extra
   (let ((commit "eb9c24b8a89bd9c58bf3238ffae7d7f385984768")
@@ -19,23 +20,29 @@
                  (base32 "13zjspxlsz70h3cw7vl8q9m2ajlh4mda29fqwin5m13xvd3r7czv"))))
       (build-system copy-build-system)
       (arguments
-       `(#:install-plan
-         '(,@(apply append
-                    (map (lambda (script)
-                           (list
-                            (list (simple-format #f "scripts/~A/~A" script script)
-                                  "bin/")
-                            (list (simple-format #f "scripts/~A/README.md" script)
-                                  (simple-format #f "share/doc/nsxiv-extra/~A/" script))))
-                         '("nsxiv-anti-alias"
-                           "nsxiv-env"
-                           "nsxiv-fill"
-                           "nsxiv-open"
-                           "nsxiv-pipe"
-                           "nsxiv-rifle"
-                           "nsxiv-saver"
-                           "nsxiv-thumb"
-                           "nsxiv-url"))))))
+       (list #:install-plan
+             ;; No need to use `with-imported-modules' because `(srfi srfi-1)'
+             ;; and `(ice-9 ftw)' are both Guile internal modules.
+             #~(begin
+                 (use-modules (srfi srfi-1))
+                 (append-map
+                  (lambda (script)
+                    (let* ((src (string-append "scripts/" script "/"))
+                           (stripped (strip-store-file-name #$output))
+                           (doc (simple-format #f "share/doc/~A/~A/"
+                                               stripped script)))
+                      (list
+                       (list (string-append src script) "bin/")
+                       (list (string-append src "README.md") doc))))
+                  '("nsxiv-anti-alias"
+                    "nsxiv-env"
+                    "nsxiv-fill"
+                    "nsxiv-open"
+                    "nsxiv-pipe"
+                    "nsxiv-rifle"
+                    "nsxiv-saver"
+                    "nsxiv-thumb"
+                    "nsxiv-url")))))
       (home-page "https://codeberg.org/nsxiv/nsxiv-extra")
       (synopsis "Extra scripts for nsxiv")
       (description "This package provides scripts which add new and commonly

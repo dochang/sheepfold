f444759800268682b232eb11e37fe0fd6642f299
Wade Zhang <dochang@gmail.com>
2023-05-08 13:41:20 +0800

    Add zst.

--- src/sheepfold/packages/compression.scm
+++ src/sheepfold/packages/compression.scm
@@ -6,6 +6,8 @@
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages version-control)
+  #:use-module (guix build-system cmake)
+  #:use-module (gnu packages compression)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define-public bzip3
@@ -59,3 +61,36 @@ string matching and PPM-style context modeling.
 Like its ancestor, BZip3 excels at compressing text or code.")
     (license (list license:gpl3
                    license:asl2.0))))
+
+(define-public zst
+  (package
+    (name "zst")
+    (version "0.4")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/kilobyte/zst")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32 "0hsg6nvg54692cp3hr6lpnb316bjkjmkvq281xj98d54n0fp2xyf"))))
+    (build-system cmake-build-system)
+    (inputs
+     (list zlib
+           bzip2
+           xz
+           `(,zstd "lib")
+           bzip3))
+    (native-inputs ;; For testing
+     (list gzip
+           bzip2
+           xz
+           zstd
+           bzip3))
+    (home-page "https://github.com/kilobyte/zst")
+    (synopsis "Compress or decompress .zst/.bz2/.gz/.xz files")
+    (description "The zst command can reduce the size of files by using a
+number of popular compression algorithms; in this version these are zstd,
+bzip2, xz, gzip, bzip3 -- and decompress them back.")
+    (license (list license:gpl2+
+                   license:bsd-1))))
